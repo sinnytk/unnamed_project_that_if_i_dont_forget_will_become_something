2261970b218c2d479d46b8a766017ce7ff7f51fb
Tarun Kumar <tarun@stitch3d.com>
2023-02-27 23:04:39 +0100

    init

---
+++ routes/index.tsx
@@ -0,0 +1,18 @@
+import { Head } from "$fresh/runtime.ts";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>i think</title>
+      </Head>
+      <body class="min-h-screen bg-gray-900 text-white">
+        <div class="p-4 mx-auto max-w-screen-md">
+          <p class="text-sm">
+            therefore i am?
+          </p>
+        </div>
+      </body>
+    </>
+  );
+}
